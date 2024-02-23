d8deb7807b901a17f38dd8bfbd7a47a4452d1838
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-02-23 19:45:57 +0530

    button chnage

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    header("Location: /public/client/login.html");
+    exit; 
+?>
--- public/js/client/register.js
+++ public/js/client/register.js
@@ -5,3 +5,4 @@ btn.addEventListener("click", active);
 function active() {
   btn.classList.toggle("is_active");
 }
+
